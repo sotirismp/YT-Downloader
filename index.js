222fa0ab559cfb10c76046552e6a8ba1369ebbd2
sotiris balas <sotirismpalas@gmai.com>
2023-01-26 13:02:10 +0200

    fixed some files

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
-app.use(express.static("build"));
+app.use(express.static("public"));
 app.use("/info", InfoRoute);
 app.use("/download", DownloadRoute);
 
